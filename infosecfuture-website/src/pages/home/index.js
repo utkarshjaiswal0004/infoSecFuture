839f7d6d66ecf3968b57eb7ae218569f5540188d
Utkarsh Jaiswal <utkarsh.jaiswal0004@gmail.com>
2023-09-08 13:25:03 +0530

    added explore section

--- infosecfuture-website/src/pages/home/index.js
+++ infosecfuture-website/src/pages/home/index.js
@@ -1,9 +1,11 @@
 import HeroSection from '../../components/home_page/hero_section/hero_section';
+import ExploreSection from '../../components/home_page/explore_section/explor_section';
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <ExploreSection />
     </>
   );
 }
